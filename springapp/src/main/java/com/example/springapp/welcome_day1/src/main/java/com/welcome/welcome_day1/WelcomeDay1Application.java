93ae636e14f63bd71ff8b524e989438c67ff3fc0
Iamneo <techadmin@iamneo.ai>
2023-08-17 05:53:08 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/springapp/welcome_day1/src/main/java/com/welcome/welcome_day1/WelcomeDay1Application.java
@@ -0,0 +1,13 @@
+package com.welcome.welcome_day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeDay1Application.class, args);
+	}
+
+}
---
+++ springapp/src/main/java/com/example/springapp/welcome_day1/src/test/java/com/welcome/welcome_day1/WelcomeDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welcome.welcome_day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeDay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
