974da8266c83fddf6fcb2ecf0faf71777c4d85e0
Iamneo <techadmin@iamneo.ai>
2023-08-17 07:02:07 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/demo/src/main/java/com/nithya/demo/Controllers/ApiController.java
@@ -0,0 +1,13 @@
+package com.nithya.demo.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    
+    @GetMapping("/welcome") 
+        public String Display(){
+            return "Welcome";
+        }
+    
+}
